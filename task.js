34c7751ef2b2155d029ed923bd6fbbb83de6218a
Alex hitsaaja <sto98@inbox.ru>
2023-10-30 10:32:50 +0200

    Cheange readme task.js

--- task.js
+++ task.js
@@ -1,9 +1,22 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  
-  return arr;
-}
+  let discriminant = (b ** 2 - (4 * a * c));
+  if (discriminant < 0){
+    console.log('Дискриминант меньше нуля. \nКорней нет!');
+    return arr;
+  }
+  else if (discriminant === 0){
+    arr.push(-b / (2 * a)) 
+    console.log('Дискриминант равен ' + discriminant + '\nКорень уравнения равен ' + arr[0])
+    
+    }
+  else if (discriminant > 0){
+  arr.push(- b + Math.sqrt(discriminant)) / (2 * a) && arr.push(- b - Math.sqrt(discriminant)) / (2 * a)
+    console.log('Дискриминант равен ' + discriminant + '\nПервый крень уравнения равен ' + arr[0] + '\nВторой корень уравнения равен ' + arr[1]);
+  } 
+    return arr;
+};
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
   
